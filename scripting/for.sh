cfedaab1794cc339caf1a38c126e4960edd42212
chethuuu <chethan.halappa18@gmail.com>
2023-02-23 11:42:09 +0000

    updated for.sh

---
+++ scripting/for.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+:for myvar in {1..5}
+:do
+:echo  "Hello world"
+:done
+
+:for  myvar in 1 2 3 a
+:do
+:echo "Hello world $myvar"
+:done
+#nested for loop
+for i in {1..5}
+do
+
+for var in {a..b}
+do
+echo "--->$i"
+echo "---->$var"
+done
+done
